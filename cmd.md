8c1717b202058b800c527d4c469a749058afaf9b
TejasMahajan02 <tejasmahajan475@gmail.com>
2024-07-23 00:45:34 +0530

    added resource

---
+++ cmd.md
@@ -0,0 +1,11 @@
+```sh
+npm install -g @nestjs/cli            
+nest new my-nest-project
+cd my-nest-project
+npm run start:dev
+npm install --save @nestjs/typeorm typeorm pg
+# npm install --save @nestjs/typeorm typeorm mysql2
+# nest g controller user
+nest g resource 
+# resource_name : users > layer : REST API > CRUD : Y
+```
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserController } from './user/user.controller';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 3306,
+      username: 'postgres',
+      password: 'root',
+      database: 'postgres',
+      entities: [],
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
+  controllers: [AppController, UserController],
   providers: [AppService],
 })
 export class AppModule {}
